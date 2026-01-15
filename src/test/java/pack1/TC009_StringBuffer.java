8e547f4e94e64bb64aa3d6b1fbd0b4bc9d885c70
Tanisha <tanishaagarwal2317@gmail.com>
2026-01-15 14:35:54 +0530

    15jan

--- src/test/java/pack1/TC009_StringBuffer.java
+++ src/test/java/pack1/TC009_StringBuffer.java
@@ -1,6 +1,6 @@
 package pack1;
 
-public class TC008_StringBuffer {
+public class TC009_StringBuffer {
        public static void main(String args[]) {
     	   StringBuffer sb = new StringBuffer("Hello");
     	   System.out.println(sb.reverse());
