ec6aed80886a78054f0034d120e106a334a35564
Tanisha <tanishaagarwal2317@gmail.com>
2026-01-16 11:02:55 +0530

    16jan

---
+++ src/test/java/pack1/TC_Interface_Implement.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class TC_Interface_Implement {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+calc c=new calc();
+System.out.println(c.add(10,20));
+}
+}
