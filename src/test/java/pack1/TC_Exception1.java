39a9713b1dfb052c2bec188c59df34598020dafe
Tanisha <tanishaagarwal2317@gmail.com>
2026-01-17 12:19:05 +0530

    17jan

---
+++ src/test/java/pack1/TC_Exception1.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class TC_Exception1 {
+	
+	try {
+		int d = 0;
+		int a = 50/d;
+	}
+	catch(Exception e) {
+		System.out.println(e.getMessage());
+	}
+	
+	
+	System.out.println("Welcome to java");
+}
+}
